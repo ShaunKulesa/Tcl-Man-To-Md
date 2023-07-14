333bc5d1ae2d32612aadce691b2bac68740b5dd8
Shaun Kulesa <69469909+ShaunKulesa@users.noreply.github.com>
2023-07-14 11:53:05 +0100

    Functional style and code comments

--- man-to-md/man_reformat.py
+++ man-to-md/man_reformat.py
@@ -1,9 +1,31 @@
-import os
 import glob
 
-for filename_dir in glob.glob("source\*"):
-    print(filename_dir)
+def fix_code_block(source: str):
+    # The man .CS and .CE create 5 spaces whereas md uses 4 spaces for a code block
 
+    """
+    Output before:
+    >     proc yieldm {value} {
+    >         yieldto return -level 0 $value
+    >     }
+    """
+
+    """
+    Output after:
+        proc yieldm {value} {
+            yieldto return -level 0 $value
+        }
+    """
+    
+    # replace .CS with .nf in source files
+    source = source.replace(".CS", ".nf")
+
+    # replace .CE with .fi in source files
+    source = source.replace(".CE", ".fi")
+
+    return source
+
+for filename_dir in glob.glob("source\*"):
     filename = filename_dir.split("\\")[-1]
     extension = filename.split(".")[-1]
 
@@ -14,11 +36,7 @@ for filename_dir in glob.glob("source\*"):
         with open(filename_dir, "r") as f:
             source = f.read()
 
-            # replace .CS with .nf in source files
-            source = source.replace(".CS", ".nf")
-
-            # replace .CE with .fi in source files
-            source = source.replace(".CE", ".fi")
+            source = fix_code_block(source)
         
         with open(filename_dir, "w") as f:
             f.write(source)
